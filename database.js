499fe1bb25fcb52709e8bb5ab9b3e2fd63ca2401
Fran-Rost <fran.rostagno2008@gmail.com>
2026-02-19 11:43:37 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 /* =================== Supabase Adapter =================== */
 
 const SUPABASE_URL = "https://arknvnqdjqmfvmrkguyz.supabase.co";
-const SUPABASE_KEY = "TU_PUBLIC_ANON_KEY_AQUI";
+const SUPABASE_KEY = "sb_publishable_ZEfS7rs0k7A6rq-2Qu_3eA_i-Bsa7b6";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
@@ -136,3 +136,4 @@ window.eliminarTorneoFederadoDB = eliminarTorneoFederadoDB;
 
 window.crearTorneoInterno = crearTorneoInterno;
 window.eliminarTorneoInternoDB = eliminarTorneoInternoDB;
+
